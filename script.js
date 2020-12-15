b1a1e3f3262fcd95fc588a1b3352ef3cda829175
Ryan Hawkins <rkhawkins80@hotmail.com>
2020-12-15 19:06:38 +0000

    Started outlining roulette logic

---
+++ script.js
@@ -0,0 +1,14 @@
+
+// Retrieve placement selection
+
+// Retrieve bet amount(s)
+// Remove bet amount total from bank
+
+// Spin roulette wheel
+// Get result from "wheel"
+
+// Check for wins
+
+// Award wins, depending on selection group payout
+
+
