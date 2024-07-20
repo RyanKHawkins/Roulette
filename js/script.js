701597390bec45466ad613fa05c101765317e02a
Ryan Hawkins <rkhawkins80@hotmail.com>
2024-07-20 15:03:57 -0500

    removed unecessary comments and extra code

--- js/script.js
+++ js/script.js
@@ -52,26 +52,6 @@ function selectBetPlacement(e) {
     return betPlacement
 }
 
-// Select bet amount
-// Confirm betAmount amount available from bank
-// function setBetAmount() {
-//     if (bettingAllowed && betPlacement && betAmount.value <= bankBalance) {
-//         let betAmountDisplay = betAmount.value;
-//         if (betAmountDisplay[0] == "0") {
-//             betAmountDisplay = betAmountDisplay.slice(1)
-//         }
-//         betDisplay_p.innerHTML = `Bet:  ${betAmountDisplay} on ${betPlacement}`;
-//         console.log(`Bet:  ${betAmountDisplay} on ${betPlacement}`);
-//     } else if (!bettingAllowed) {
-//         betDisplay_p.innerHTML = "Hold your bets."
-//     } else if (betAmount.value > bankBalance) {
-//         betDisplay_p.innerHTML = "You do not have enough money to make that bet.";
-//         betAmount.value = 0;
-//     } else {
-//         betDisplay_p.innerHTML = "You must select a bet placement."
-//     }
-// }
-
 function isValidBet() {
     if (betAmount.value > bankBalance) {
         console.log(betAmount.value, bankBalance);
@@ -128,14 +108,12 @@ function getWheelIndex() {
     return Math.floor(Math.random() * wheel.length)
 }
 
-// Displays in the console
 // TODO:  Scale down
 function displayWheelResult() {
     const color = getResultColor(wheelResult);
-    // messageDisplay_p.innerText = `${color} ${wheel[wheelResult]}`
     messageDisplay_p.innerHTML = `<p style="color: ${getResultColor(wheelResult)}">${wheelResult}</p>`
     console.log(`${color} ${wheelResult}`)
-    displayPreviousResults()
+    displayPreviousResults();
     console.log("display wheel result")
 }
 
@@ -153,13 +131,9 @@ function getResultColor(result) {
 
 // Add result to list of results
 function displayPreviousResults() {
-    resultsList.push(wheelResult)
+    resultsList.push(wheelResult);
     resultsList = resultsList.slice(-10);
     console.log("resultsList: ", resultsList);
-    prevResults_span.innerHTML = ""
-    // let resultsDisplay = resultsList.forEach((result) => {
-    //     prevResults_span.append(` ${result}`)
-    // })
     prevResults_span.innerHTML = "";
     resultsList.forEach(result => {
         prevResults_span.innerHTML +=` <span style="color: ${getResultColor(result)}">${result}</span>`
@@ -168,7 +142,7 @@ function displayPreviousResults() {
 
 // Check for wins by payout (switch message?)
 function checkForWins(result) {
-    console.log(`Bet selection: ${betPlacement}`)
+    console.log(`Bet selection: ${betPlacement}`);
     // Check for inside bets won
     if (betPlacement == result && numbers.includes(betPlacement)) {
         bankBalance += betAmount * 35;
@@ -176,15 +150,15 @@ function checkForWins(result) {
     // Check for outside bets won
     if (["red", "black"].includes(betPlacement)) {
         if (betPlacement == "red" && red.includes(result)) {
-            bankBalance += betAmount * 2
+            bankBalance += betAmount * 2;
         }
         if (betPlacement == "black" && black.includes(result)) {
-            bankBalance += betAmount * 2
+            bankBalance += betAmount * 2;
         }
     }
     if (["even", "odd"].includes(betPlacement)) {
         if (betPlacement == "even" && result % 2 == 0 || betPlacement == "odd" && result % 2 != 0) {
-            bankBalance += betAmount * 2            
+            bankBalance += betAmount * 2;
         }
     }
     if (["first12", "second12", "third12"].includes(betPlacement)) {
@@ -217,7 +191,7 @@ function checkForWins(result) {
             bankBalance += betAmount * 2;
         }
     }
-    console.log("check for wins")
+    console.log("check for wins");
     console.log("new balance: ", bankBalance);
 }
 
@@ -234,18 +208,15 @@ function payOutWins() {
 
 // Display win or lost
 
-// Reset table
 function resetTable() {
 
     betDisplay_p.innerText = "Place your bet."
     // messageDisplay_p.innerText = "Play again.";
 
     bettingAllowed = true; // Reenable betting
-    resetBets()
+    resetBets();
     spinBtn.addEventListener("click", spinWheel);
-    spinBtn.innerText = "Spin Wheel"
+    spinBtn.innerText = "Spin Wheel";
     // betBtn.addEventListener("click", setBetAmount)
     console.log("reset table\n")
-
-
 }
